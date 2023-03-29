822fab9f3a20bd1a9de8f6e639def6a4021afe02
RuituLyu <lvruitu@gmail.com>
2023-03-29 01:33:31 -0500

    newest commit

--- scripts/map_peakscalling.sh
+++ scripts/map_peakscalling.sh
@@ -7,16 +7,16 @@ set -e
 ## Read arguments                                                     
 usageHelp="Usage: KAS-Analyzer peakscalling [ -h ] [ -m peaks caller ] [ -k KAS-seq ] [ -c Control ] [ -b mode ] [ -o prefix ] [ -p pvalue or qvalue ] [ -g assembly id ]."
 exampleHelp="Example: nohup KAS-Analyzer peakscalling -k KAS-seq.rep1.bed,KAS-seq.rep2.bed -c Control_Input.rep1.bed,Control_Input.rep2.bed -o KAS-seq -g hg19 &"
-peakscallerHelp="-m [peaks caller]: please input the peaks caller (macs14, macs2) that you want to use for KAS-seq peaks calling. DEFAULT: macs2."
+peakscallerHelp="-m [peaks caller]: please input the peaks caller (macs2, epic2 or macs2_and_epic2) that you want to use for KAS-seq peaks calling. DEFAULT: macs2_and_epic2."
 KASseqHelp="-k [KAS-seq]: please input the KAS-seq bed or bam files. e.g. KAS-seq.rep1.bed,KAS-seq.rep2.bed or KAS-seq.rep1.bam,KAS-seq.rep2.bam. REQUIRED."
 controlHelp="-c [Control]: please input the KAS-seq control bed or bam files. e.g. KAS-seq_Input.rep1.bed,KAS-seq_Input.rep2.bed or KAS-seq_Input.rep1.bam,KAS-seq_Input.rep2.bam. OPTIONAL."
-modeHelp="-b [mode]: specify macs2 to perferm KAS-seq peaks calling with 'broad' or 'sharp' mode. -b option only works for macs2. DEFAULT: broad."
+modeHelp="-b [mode]: specify macs2 to perferm KAS-seq peaks calling with 'broad', 'sharp' or 'both' mode. epic2 for broad peaks; macs2 for sharp peaks; combined epic2&macs2 for broad and sharp peaks . DEFAULT: both."
 prefixHelp="-o [prefix]: please input the prefix (basename), which will be used to generate the name of 'KAS-Analyzer peakscalling' output files. REQUIRED."
-cutoffHelp="-p [pvalue or qvalue]: please input the pvalue or qvalue for KAS-seq peaks calling with macs14 or macs2. DEFAULT: macs14: 1e-7; macs2: 0.01"
+cutoffHelp="-p [FDR or qvalue]: please input the pvalue or qvalue for KAS-seq peaks calling with macs14 or macs2. DEFAULT: epic2 FDR: 0.05; macs2 qvalue: 0.01"
 assemblyidHelp="-g [assembly id]: please specify the reference genome assembly id of KAS-seq data. e.g. Human: hg18, hg19, hg38; Mouse: mm9, mm10, mm39; C.elegans: ce10, ce11; D.melanogaster: dm3, dm6; Rat: rn6, rn7; Zebra fish: danRer10, danRer11. REQUIRED."
 # genome size. e.g. human(hs): 2.7e9; mouse(mm): 1.87e9; C.elegans(ce): 9e7; fruitfly(dm): 1.2e8; rat(rn): 2.5e9; zebrafish(danRer): 1e9.
 helpHelp="-h: print this help and exit.
-Note: This shell script mainly invoke macs14 or macs2 for calling (sp)KAS-seq data peaks, please google their official websites for more information."
+Note: This shell script mainly invoke macs2 and epic2 for calling (sp)KAS-seq data peaks, please google their github pages for more information."
 
 # print help function.
 printHelpAndExit() {
@@ -64,11 +64,19 @@ while getopts 'hm:k:c:b:o:p:g:' opt; do
     esac
 done
 
-# check if macs2 was installed.
+# check if macs2\epic2\macs14 was installed.
 if ! type macs2 > /dev/null 2>&1 ;then
    echo "macs2 was not installed or not export to the \$PATH'"
    echo ""
    echo "Install macs2 with 'conda install -c bioconda macs2' or refer the official website of 'macs2'."
+   echo ""
+   exit 1
+fi
+
+if ! type epic2 > /dev/null 2>&1 ;then
+   echo "epic2 was not installed or not export to the \$PATH'"
+   echo ""
+   echo "Install epic2 with 'conda install -c bioconda epic2=0.0.52' or refer the official website of 'epic2'."
    echo ""
    exit 1
 fi
@@ -119,106 +127,203 @@ fi
 
 # setup the default options.
 if test -z $peakscaller ;then
-   peakscaller="macs2"
+   peakscaller="macs2_and_epic2"
 fi
 
-if test -z $mode && [[ $peakscaller == "macs2" ]] ;then 
-    mode="broad" 
+if test -z $mode ;then
+   peakscaller="both"
+fi
+
+if [[ $peakscaller == "epic2" ]] ;then 
+    mode="broad"	
+elif [[ $mode == "broad" ]] ;then
+    peakscaller="epic2"	
 fi 
 
+if [[ $peakscaller == "macs2" ]] ;then
+    mode="sharp"
+elif [[ $mode == "sharp" ]] ;then
+    peakscaller="macs2"
+fi
 
-if test -n "$mode" && [[ $mode != "broad" ]] && [[ $mode != "sharp" ]] ;then
+if [[ $peakscaller == "macs2_and_epic2" ]] ;then
+   mode="both"
+elif [[ $mode == "both" ]] ;then
+   peakscaller="macs2_and_epic2"
+fi
+
+# test unsuported peak calling mode.
+if test -n "$mode" && [[ $mode != "broad" ]] && [[ $mode != "sharp" ]] && [[ $mode != "both" ]] ;then
     echo ""
-    echo "Error: unsupported peak calling mode using macs2: $mode. e.g. broad or sharp. Default: broad."
+    echo "Error: unsupported peak calling mode: $mode. e.g. sharp, broad or both. DEFAULT: both."
     echo ""
     exit -1
 fi
 
 # test unsuported peak caller.
-if [[ $peakscaller != "macs2" ]] && [[ $peakscaller != "macs14" ]] ;then
+if [[ $peakscaller != "macs2" ]] && [[ $peakscaller != "epic2" ]] && [[ $peakscaller != "macs2_and_epic2" ]] ;then
    echo ""
-   echo "Error: unsupported peak caller: $peakscaller. e.g. macs14 or macs2. Default: macs2."
+   echo "Error: unsupported peak caller: $peakscaller. e.g. macs2 or epic2 or macs2_and_epic2. DEFAULT: macs2_and_epic2."
    echo " "
    exit -1
 fi
+
+# get the path of 'KAS-Analyzer KAS-seq' shell script.
+SH_SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 # setup the default pvalue or qvalue.
 if test -z $cutoff ;then
    if [[ $peakscaller == "macs2" ]] ;then
       cutoff="0.01"
-   elif [[ $peakscaller == "macs14" ]] ;then
-      cutoff="1e-7"
+   elif [[ $peakscaller == "epic2" ]] ;then
+      cutoff="0.05"
+   elif [[ $peakscaller == "macs2_and_epic2" ]] ;then
+      macs2_cutoff="0.01"
+      epic2_cutoff="0.05"      
    fi	
 fi
 
 # call KAS-seq peaks without control using macs2
-if test -z $control && [[ $peakscaller == "macs2" ]] && [[ $mode == "broad" ]] ;then	
+if test -z $control && [[ $peakscaller == "macs2" ]] && [[ $mode == "sharp" ]] ;then	
 
    echo $KASseq > .KASseq.txt
    sed -i "s/\,/ /g" .KASseq.txt
    KASseq_list=$(cat .KASseq.txt)
-   echo "Call (sp)KAS-seq peaks for $KASseq_list at broad mode using macs2 ..." 
-   echo ""
-   macs2 callpeak -t $KASseq_list -n $prefix --broad -g $genomesize --broad-cutoff $cutoff -q $cutoff 2> ${prefix}_output_${peakscaller}.log
-   rm -f .KASseq.txt
-   echo "done."
-   echo ""
-
-elif test -z $control && [[ $peakscaller == "macs2" ]] && [[ $mode == "sharp" ]] ;then
-   echo $KASseq > .KASseq.txt
-   sed -i "s/\,/ /g" .KASseq.txt
-   KASseq_list=$(cat .KASseq.txt)
-   echo "Call (sp)KAS-seq peaks for $KASseq_list at sharp mode using macs2 ..." 
+   echo "Call sharp (sp)KAS-seq peaks for $KASseq_list using macs2 ..." 
    echo ""
    macs2 callpeak -t $KASseq_list -n $prefix -g $genomesize -q $cutoff 2> ${prefix}_output_${peakscaller}.log
    rm -f .KASseq.txt
    echo "done."
    echo ""
 
-# call KAS-seq peaks with control using macs2
-elif test -n "$control" && [[ $peakscaller == "macs2" ]] && [[ $mode == "broad" ]] ;then
+elif test -z $control && [[ $peakscaller == "epic2" ]] && [[ $mode == "broad" ]] ;then
    echo $KASseq > .KASseq.txt
-   echo $control > .control.txt
    sed -i "s/\,/ /g" .KASseq.txt
-   sed -i "s/\,/ /g" .control.txt
    KASseq_list=$(cat .KASseq.txt)
-   control_list=$(cat .control.txt)
-   echo "Call (sp)KAS-seq peaks for $KASseq_list compaired to $control_list at broad mode using macs2 ..." 
+   echo "Call broad (sp)KAS-seq peaks for $KASseq_list using epic2 ..." 
    echo ""
-   macs2 callpeak -t $KASseq_list -c $control_list -n $prefix --broad -g $genomesize --broad-cutoff $cutoff -q $cutoff 2> ${prefix}_output_${peakscaller}.log
+   epic2 -t $KASseq_list --output $prefix --chromsizes ${SH_SCRIPT_DIR}/../chrom_size/${assemblyid}.chrom.sizes --fragment-size 300 --false-discovery-rate-cutoff $cutoff 2> ${prefix}_output_${peakscaller}.log
    rm -f .KASseq.txt
-   rm -f .control.txt
    echo "done."
    echo ""
 
-elif test -n "$control" && [[ $peakscaller == "macs2" ]] && [[ $mode == "sharp" ]] ;then	
+elif test -z $control && [[ $peakscaller == "macs2_and_epic2" ]] && [[ $mode == "both" ]] ;then
+
+   echo "Control(Input) data were not provided, call sharp and broad KAS-seq peaks using both macs2 and epic2 can't be performed"
+   echo ""
+   exit -1
+
+# call KAS-seq peaks with control using macs2 and epic2.
+elif test -n "$control" && [[ $peakscaller == "macs2" ]] && [[ $mode == "sharp" ]] ;then
    echo $KASseq > .KASseq.txt
    echo $control > .control.txt
    sed -i "s/\,/ /g" .KASseq.txt
    sed -i "s/\,/ /g" .control.txt
    KASseq_list=$(cat .KASseq.txt)
    control_list=$(cat .control.txt)
-   echo "Call (sp)KAS-seq peaks for $KASseq_list compared to $control_list at sharp mode using macs2 ..."
+   echo "Call sharp (sp)KAS-seq peaks for $KASseq_list compaired to $control_list using macs2 ..." 
    echo ""
    macs2 callpeak -t $KASseq_list -c $control_list -n $prefix -g $genomesize -q $cutoff 2> ${prefix}_output_${peakscaller}.log
+
+   grep ^chr ${prefix}_peaks.xls > ${prefix}_macs2_peaks.bed
+
+   rm -f ${prefix}_model.r
+   rm -f ${prefix}_summits.bed
+   rm -f ${prefix}_peaks.narrowPeak
+   rm -f ${prefix}_peaks.xls
    rm -f .KASseq.txt
    rm -f .control.txt
    echo "done."
    echo ""
 
-# call KAS-seq peaks without control using macs14
-elif test -z $control && [[ $peakscaller == "macs14" ]] ;then
-   echo "Call (sp)KAS-seq peaks for $KASseq_list using macs2 ..."
+elif test -n "$control" && [[ $peakscaller == "epic2" ]] && [[ $mode == "broad" ]] ;then	
+   echo $KASseq > .KASseq.txt
+   echo $control > .control.txt
+   sed -i "s/\,/ /g" .KASseq.txt
+   sed -i "s/\,/ /g" .control.txt
+   KASseq_list=$(cat .KASseq.txt)
+   control_list=$(cat .control.txt)
+   echo "Call broad (sp)KAS-seq peaks for $KASseq_list compared to $control_list using epic2 ..."
    echo ""
-   macs14 -t $KASseq -n $prefix -p $cutoff 2> ${prefix}_output_${peakscaller}.log
+   epic2 -t $KASseq_list -c $control_list --output $prefix --chromsizes ${SH_SCRIPT_DIR}/../chrom_size/${assemblyid}.chrom.sizes --fragment-size 300 --false-discovery-rate-cutoff $cutoff 2> ${prefix}_output_${peakscaller}.log
+
+   sed "1d" $prefix | awk 'BEGIN {print "#Chromosome\tStart\tEnd\tPValue\tScore\tChIPCount\tInputCount\tFDR\tlog2FoldChange"} $10>=0.58 {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$3,$4,$5,$7,$8,$9,$10)}' > ${prefix}_epic2_peaks.bed
+   
+   rm -f $prefix
+   rm -f .KASseq.txt
+   rm -f .control.txt
    echo "done."
    echo ""
 
-# call KAS-seq peaks with control using macs14	
-elif test -n "$control" && [[ $peakscaller == "macs14" ]] ;then
-   echo "Call (sp)KAS-seq peaks for $KASseq compared to $control using macs2 ..."
+elif test -n "$control" && [[ $peakscaller == "macs2_and_epic2" ]] && [[ $mode == "both" ]] ;then
+   echo $KASseq > .KASseq.txt
+   echo $control > .control.txt
+   sed -i "s/\,/ /g" .KASseq.txt
+   sed -i "s/\,/ /g" .control.txt
+   KASseq_list=$(cat .KASseq.txt)
+   control_list=$(cat .control.txt)	
+ 
+   echo "Call broad and sharp (sp)KAS-seq peaks for $KASseq_list compared to $control_list using epic2 and macs2 ..." 
+
+   echo "Call broad (sp)KAS-seq peaks for $KASseq_list compared to $control_list using epic2 ..."
+   echo ""  
+   epic2 -t $KASseq_list -c $control_list --output $prefix --chromsizes ${SH_SCRIPT_DIR}/../chrom_size/${assemblyid}.chrom.sizes --fragment-size 300 --false-discovery-rate-cutoff $epic2_cutoff 2> ${prefix}_output_epic2.log
+
+   sed "1d" $prefix | awk '$10>=0.58 {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$3,$4,$5,$7,$8,$9,$10,"peaks"NR)}' > ${prefix}_epic2_peaks.bed
+   echo "done."
    echo ""
-   macs14 -t $KASseq -c $control -n $prefix -p $cutoff 2> ${prefix}_output_${peakscaller}.log
+
+   echo "Call sharp (sp)KAS-seq peaks for $KASseq_list compared to $control_list using macs2 ..."
+   echo ""
+   macs2 callpeak -t $KASseq_list -c $control_list -n $prefix -g $genomesize -q $macs2_cutoff 2> ${prefix}_output_macs2.log
+
+   grep ^chr ${prefix}_peaks.xls | sed "1d" > ${prefix}_macs2_peaks.bed
+   echo "done."
+   echo ""
+
+   echo "Filter macs2 KAS-seq peaks with at least 5 fold change compared to its relative input control ..."
+   echo ""
+   awk '$8>=5 {print $0}' ${prefix}_macs2_peaks.bed | awk '{if ($4<1000 && $2-1000>=0) printf("%s\t%d\t%d\t%s\t%d\t%d\t%s\t%d\t%d\n",$1,$2,$3,$1,$2-1000,$2,$1,$3,$3+1000); else if ($4>=1000 && $2-3000>=0) printf("%s\t%d\t%d\t%s\t%d\t%d\t%s\t%d\t%d\n",$1,$2,$3,$1,$2-3000,$2,$1,$3,$3+3000)}' > ${prefix}_macs2_peaks.middle.left.right.3bed
+
+   cat $KASseq_list | sortBed -i > ${prefix}_KAS-seq_reads.bed
+
+   intersectBed -a ${prefix}_macs2_peaks.middle.left.right.3bed -b ${prefix}_KAS-seq_reads.bed -wa -F 0.5 -c | awk '{printf("%s\t%d\t%d\t%s\t%d\t%d\t%s\t%d\t%d\t%d\n",$4,$5,$6,$1,$2,$3,$7,$8,$9,$10)}' | intersectBed -a - -b ${prefix}_KAS-seq_reads.bed -wa -F 0.5 -c | awk '{printf("%s\t%d\t%d\t%s\t%d\t%d\t%s\t%d\t%d\t%d\t%d\n",$7,$8,$9,$4,$5,$6,$1,$2,$3,$10,$11)}' | intersectBed -a - -b ${prefix}_KAS-seq_reads.bed -wa -F 0.5 -c | awk '{if ($6-$5>=1000) printf("%s\t%d\t%d\t%.3f\t%.3f\t%.3f\n",$4,$5,$6,$10/($6-$5),$11/3000,$12/3000); else if ($6-$5<1000) printf("%s\t%d\t%d\t%.3f\t%.3f\t%.3f\n",$4,$5,$6,$10/($6-$5),$11/1000,$12/1000)}' | awk '$4>=$5+$6 {printf("%s\t%d\t%d\n",$1,$2,$3)}' > ${prefix}_macs2_peaks.filter.3bed
+  
+   rm -f $prefix 
+   
+   rm -f ${prefix}_model.r
+   rm -f ${prefix}_summits.bed
+   rm -f ${prefix}_peaks.narrowPeak
+   rm -f ${prefix}_peaks.xls
+   
+   rm -f ${prefix}_macs2_peaks.middle.left.right.3bed
+   rm -f ${prefix}_KAS-seq_reads.bed
+
+   rm -f .KASseq.txt
+   rm -f .control.txt
+   
+   echo "done."
+   echo ""
+   
+
+   echo "Output sharp KAS-seq peaks ..."
+   echo ""
+
+   intersectBed -a ${prefix}_macs2_peaks.bed -b ${prefix}_macs2_peaks.filter.3bed -wa -f 1 | intersectBed -a - -b ${prefix}_epic2_peaks.bed -wa -f 0.5 | sortBed -i | awk 'BEGIN {print "#chr\tstart\tend\tlength\tabs_summit\tpileup\t-log10(pvalue)\tfold_enrichment\t-log10(qvalue)"} {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9)}' > ${prefix}_KAS-seq_peaks.sharp.bed
+
+   rm -f ${prefix}_macs2_peaks.bed
+   rm -f ${prefix}_macs2_peaks.filter.3bed
+   rm -f ${prefix}_output_macs2.log 
+   echo "done."
+   echo ""
+
+   echo "Output broad KAS-seq peaks ..."
+   echo ""
+   bedtools subtract -a ${prefix}_epic2_peaks.bed -b ${prefix}_KAS-seq_peaks.sharp.bed | awk 'BEGIN {print "#Chromosome\tStart\tEnd\tPValue\tScore\tStrand\tChIPCount\tInputCount\tFDR\tlog2FoldChange"} {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10)}' > ${prefix}_KAS-seq_peaks.broad.bed
+
+   rm -f ${prefix}_epic2_peaks.bed
+   rm -f ${prefix}_output_epic2.log
+
    echo "done."
    echo ""
 
